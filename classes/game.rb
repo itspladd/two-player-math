9d09eede8b5f6b35c72e825f4e2bcac9c80a2a14
Paul Ladd <vlendle@gmail.com>
2021-04-26 19:52:57 +0000

    Game now adds new Players to its @players array.

--- classes/game.rb
+++ classes/game.rb
@@ -1,7 +1,15 @@
 class Game
+  NUMBER_OF_PLAYERS = 2
+
   def initialize
-    @players = []
-    @current_player
     puts "New game lives!"
+    setup
+  end
+
+  def setup
+    # Create a new array with the proper number of spots.
+    @players = Array.new(NUMBER_OF_PLAYERS)
+    # Fill the array with new Player objects.
+    @players.fill { |i| Player.new }
   end
 end
