a16dcdc6ec7bf601138f0b3aaab9073b7bcb0d40
Paul Ladd <vlendle@gmail.com>
2021-04-26 19:17:50 +0000

    Add game_runner class and main runner loop.

--- game_runner.rb
+++ game_runner.rb
@@ -1,24 +1,57 @@
 require_relative "classes/game"
 require_relative "classes/player"
 require_relative "classes/question"
+require "pp"
 
-def handle_input(input)
-  if input.downcase == "e"
+class Game_Runner
+  def initialize()
+    @games = load_games
+    run_app
+  end
+
+  def handle_input(input)
+    if input.downcase == "e"
+      quit_app
+    end
+    if input.downcase == "s"
+      start_game
+    end
+    if input.downcase == "l"
+      list_games
+    end
+  end
+
+  def run_app
+    while true
+      puts "[S]tart a new game, [L]ist games from this session, or [E]xit?"
+      print ">>"
+      input = $stdin.gets.chomp
+      handle_input(input)
+    end
+
+  end
+
+  def quit_app
     puts "Bye!"
     exit
   end
-  if input.downcase == "s"
+
+  def start_game
     puts "Starting a new game!"
-    game = Game.new
+    add_game(Game.new)
+  end
+
+  def list_games
+    pp @games
+  end
+
+  def load_games
+    []
+  end
+
+  def add_game(game)
+    @games << game
   end
 end
 
-def run_app
-  while true
-    puts("[S]tart a new game or [E]xit?")
-    input = $stdin.gets.chomp
-    handle_input(input)
-  end
-end
-
-run_app
+game = Game_Runner.new
