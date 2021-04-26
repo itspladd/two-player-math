a1935003948c26896a43f738c0c0e90387966190
Paul Ladd <vlendle@gmail.com>
2021-04-26 21:59:47 +0000

    Remove unused features from game runner

--- classes/game_runner.rb
+++ classes/game_runner.rb
@@ -2,8 +2,16 @@ require "pp"
 
 class Game_Runner
   def initialize()
-    @games = load_games
     run_app
+  end
+
+  def run_app
+    while true
+      puts "[S]tart a new game or [E]xit?"
+      print ">>"
+      input = $stdin.gets.chomp
+      handle_input(input)
+    end
   end
 
   def handle_input(input)
@@ -13,19 +21,10 @@ class Game_Runner
     if input.downcase == "s"
       start_game
     end
-    if input.downcase == "l"
-      list_games
-    end
   end
 
-  def run_app
-    while true
-      puts "[S]tart a new game, [L]ist games from this session, or [E]xit?"
-      print ">>"
-      input = $stdin.gets.chomp
-      handle_input(input)
-    end
-
+  def start_game
+    game = Game.new
   end
 
   def quit_app
@@ -33,19 +32,4 @@ class Game_Runner
     exit
   end
 
-  def start_game
-    add_game(Game.new)
-  end
-
-  def list_games
-    pp @games
-  end
-
-  def load_games
-    []
-  end
-
-  def add_game(game)
-    @games << game
-  end
 end
